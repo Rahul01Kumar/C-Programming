494b45a64170399e354d544d2e58a5c666cf74d8
RAHUL KUMAR <114794076+Rahul01Kumar@users.noreply.github.com>
2023-03-08 01:26:19 +0530

    Add files via upload

---
+++ fibonaccifunction.c
@@ -0,0 +1,64 @@
+/*
+Fibonacci series is a sequence of Integers 
+that starts with 0 followed by 1, 
+in this sequence the first two terms 
+i.e. 0 and 1 are fixed, 
+and we get the successive terms by 
+summing up their previous last two terms. 
+i.e, the series follows a pattern that 
+each number is equal to the sum of its 
+preceding two numbers.
+
+Example: 
+first = 0 
+second  = 1
+next = first + second // 1
+0, 1, 1,
+
+
+Example: 
+0, 1, 1, 
+first = second; 
+second = next
+
+Explanation:
+0
+1
+0 + 1 = 1
+1 + 1 = 2
+2 + 1 = 3
+3 + 2 = 5
+5 + 3 = 8
+...
+
+
+*/ 
+#include <stdio.h>
+
+void fibonacci(int);
+int main()
+{
+	// variable to store how many elements to be displayed in the series
+    int num;
+	 
+    // taking user input
+    printf("Enter the number of elements to be in the series : ");
+    scanf("%d", &num);
+	 
+    fibonacci(num);    
+
+    return 0;
+}
+void fibonacci(int x)
+{
+	int i; 
+	int first = 0, second = 1,next; 
+	printf("\n%d\t%d ",first,second);
+	for ( i = 1; i <= x; i++)
+    {
+    	next = first + second;    	
+        printf("\t%d", next); 
+        first = second; 
+        second = next; 
+    }
+}
